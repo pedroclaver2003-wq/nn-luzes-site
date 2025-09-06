7944fb4b51b06d8985a6303829a3c5fcd35a7344
pedroclaver2003-wq <pedroclaver2003@gmail.com>
2025-09-06 11:51:28 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+Import React from “react”;
+Import ReactDOM from “react-dom/client”;
+Import App from “./App”;
+Import “./index.css”;
+
+ReactDOM.createRoot(document.getElementById(“root”)).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
